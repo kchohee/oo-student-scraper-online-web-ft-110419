c3e095886563ab39f29a1dcbaa53c9ac5bfd6f87
Marie (Cho) Hughes <kchohee0630@gmail.com>
2019-11-25 22:44:10 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,14 +6,13 @@ class Scraper
    def initialize(student_hash)
      student_hash.each {|k,v| self.send("#{k}=", v)}
    end
-   
+
   def self.scrape_index_page(index_url)
-    
+
   end
 
   def self.scrape_profile_page(profile_url)
-    
+
   end
 
 end
-
