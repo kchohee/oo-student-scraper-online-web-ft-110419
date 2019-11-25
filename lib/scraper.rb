f59710a3112d931e8ff7f71e9d08ea71a538b7c7
Marie (Cho) Hughes <kchohee0630@gmail.com>
2019-11-25 23:27:43 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,7 +5,7 @@ class Scraper
    attr_accessor :name, :location, :profile_quote, :bio, :twitter, :linkedin, :github, :blog, :profile_url
    @@all = []
    def initialize(student_hash)
-     @@all << student_hash.each {|k,v| self.send("#{k}",  "#{v}")}
+     @@all << student_hash.each {|k,v| self.send("#{k}=",  "#{v}")}
      binding.pry
    end
 
@@ -18,6 +18,6 @@ class Scraper
   end
 
 end
-student_joe_hash = {"twitter" =>"https://twitter.com/jmburges","linkedin"=>"https://www.linkedin.com/in/jmburges","github"=>"https://github.com/jmburges","blog"=>"http://joemburgess.com/", "profile_quote"=>"\"Reduce to a previously solved problem\"","bio"=>"I grew up outside of the Washington DC (NoVA!) and went to college at Carnegie Mellon University in Pittsburgh. After college, I worked as an Oracle consultant for IBM for a bit and now I teach here at The Flatiron School."}
+student_joe_hash = { twitter =>"https://twitter.com/jmburges", linkedin=>"https://www.linkedin.com/in/jmburges",github=>"https://github.com/jmburges",blog=>"http://joemburgess.com/", profile_quote=>"\"Reduce to a previously solved problem\"",bio=>"I grew up outside of the Washington DC (NoVA!) and went to college at Carnegie Mellon University in Pittsburgh. After college, I worked as an Oracle consultant for IBM for a bit and now I teach here at The Flatiron School."}
 
 Scraper.new(student_joe_hash)
