020a79cbb722021f2380c0e838a38f445ac34164
Marie (Cho) Hughes <kchohee0630@gmail.com>
2019-11-26 00:51:52 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,7 +7,7 @@ class Scraper
     web = Nokogiri::HTML(open(index_url))
     roster = web.css(".student-card")
     roster.each do |info|
-      student_info << {:name => card.css("h4").text, :location => card.css("p").text, :profile_url => card.css("a")[0]["href"]}
+      student_info << {:name => card.css("h4").text, :location => card.css("p").text, :profile_url => card.css("a.href")]}
     end
     student_info
   end
