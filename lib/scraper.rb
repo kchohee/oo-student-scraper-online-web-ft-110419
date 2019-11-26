2368e3ea2d03f9de9fc8cbfda609b613df6776aa
Marie (Cho) Hughes <kchohee0630@gmail.com>
2019-11-26 00:48:39 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,9 +3,13 @@ require 'pry'
 
 class Scraper
   def self.scrape_index_page(index_url)
-
-  web = Nokogiri::HTML(open("https://learn-co-curriculum.github.io/student-scraper-test-page/index.html"))
-
+    student_info=[]
+    web = Nokogiri::HTML(open(index_url))
+    roster = web.css(".student-card")
+    roster.each do |info|
+      student_info << {:name => card.css("h4").text, :location => card.css("p").text, :profile_url => card.css("a")[0]["href"]}
+    end
+    student_info
   end
 
   def self.scrape_profile_page(profile_url)
@@ -13,6 +17,3 @@ class Scraper
   end
 
 end
-student_joe_hash = { :twitter =>"https://twitter.com/jmburges", :linkedin=>"https://www.linkedin.com/in/jmburges",:github=>"https://github.com/jmburges",:blog=>"http://joemburgess.com/", :profile_quote=>"\"Reduce to a previously solved problem\"",:bio=>"I grew up outside of the Washington DC (NoVA!) and went to college at Carnegie Mellon University in Pittsburgh. After college, I worked as an Oracle consultant for IBM for a bit and now I teach here at The Flatiron School."}
-
-Scraper.new(student_joe_hash)
