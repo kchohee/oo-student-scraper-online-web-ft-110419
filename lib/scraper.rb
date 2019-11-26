b047197bbce8c113eb0774553187bd2ae0a4c5e7
Marie (Cho) Hughes <kchohee0630@gmail.com>
2019-11-26 00:52:22 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -11,9 +11,10 @@ class Scraper
     end
     student_info
   end
-
   def self.scrape_profile_page(profile_url)
 
   end
-
 end
+student_joe_hash = { :twitter =>"https://twitter.com/jmburges", :linkedin=>"https://www.linkedin.com/in/jmburges",:github=>"https://github.com/jmburges",:blog=>"http://joemburgess.com/", :profile_quote=>"\"Reduce to a previously solved problem\"",:bio=>"I grew up outside of the Washington DC (NoVA!) and went to college at Carnegie Mellon University in Pittsburgh. After college, I worked as an Oracle consultant for IBM for a bit and now I teach here at The Flatiron School."}
+
+Scraper.new(student_joe_hash)
