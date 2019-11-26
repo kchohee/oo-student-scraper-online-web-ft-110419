2f994f590dda7dfcb7033c0575741404eede221b
Marie (Cho) Hughes <kchohee0630@gmail.com>
2019-11-26 03:58:47 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -19,7 +19,7 @@ class Scraper
     profile.each do |info|
       profile_info[:profile_quote]=info.css(".vitals-text-container .profile-quote").text
       profile_info[:bio]=info.css(".description-holder p").text
-       case true
+      case true
       when info.attr("href").include?("twitter")
         profile_info[:twitter]=info.attribute('href').value
       when info.attr("href").include?("linkedin")
@@ -28,6 +28,7 @@ class Scraper
         profile_info[:github]=info.attribute('href').value
       when info.attr("href").include?("blog")
         profile_info[:blog]=info.attribute('href').value
+      end
     end
     profile_info
   end
