ddafcbe7617ed34a1964c5cf45f881fc8afccd6c
Marie (Cho) Hughes <kchohee0630@gmail.com>
2019-11-26 03:17:55 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -16,7 +16,7 @@ class Scraper
   def self.scrape_profile_page(profile_url)
     profile_info={}
     web = Nokogiri::HTML(open(profile_url)
-    profile = web.css("//div.description-holder/p[1]")
+    profile = web.css(".social-icon-container a")
     profile.each do |info|
       case info
       when info.attr("href").include?("twitter")
