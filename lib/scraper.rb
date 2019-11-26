fa5b8cd05272504f59170975d596eb728ab5d128
Marie (Cho) Hughes <kchohee0630@gmail.com>
2019-11-26 00:00:07 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,7 +5,7 @@ class Scraper
    attr_accessor :name, :location, :profile_quote, :bio, :twitter, :linkedin, :github, :blog, :profile_url
    @@all = []
    def initialize(student_hash)
-     student_hash.each {|k,v| self.send("#{k}=", "#{v}")}
+     student_hash.each {|k,v| self.send("#{k}=", v)}
      @@all << self
     binding.pry
    end
