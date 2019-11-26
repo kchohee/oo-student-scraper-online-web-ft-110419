3fcbe358d5e7497f69cf7ea8f42f98d4d50d09dc
Marie (Cho) Hughes <kchohee0630@gmail.com>
2019-11-26 03:53:44 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -20,12 +20,12 @@ class Scraper
        case true
       when info.attr("href").include?("twitter")
         profile_info[:twitter]=info.attribute('href').value
-      # when info.attr("href").include?("linkedin")
-      #   profile_info[:linkedin]=info.xpath('a').attr('href').text.strip
-      # when info.attr("href").include?("github")
-      #   profile_info[:github]=info.xpath('a').attr('href').text.strip
-      # when info.attr("href").include?("blog")
-      #   profile_info[:blog]=info.xpath('a').attr('href').text.strip
+      when info.attr("href").include?("linkedin")
+        profile_info[:linkedin]=info.attribute('href').value
+      when info.attr("href").include?("github")
+        profile_info[:github]=info.attribute('href').value
+      when info.attr("href").include?("blog")
+        profile_info[:blog]=info.attribute('href').value
       end
       profile_info[:profile_quote]=info.css(".vitals-text-container .profile-quote").text
       profile_info[:bio]=info.css(".description-holder p").text
